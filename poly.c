7aa4036e4baac0b3e5aa54e86c1f5fecc263aead
DODO3369 <wartytw@gmail.com>
2024-06-19 11:52:29 +0800

    only-reduction version

--- poly.c
+++ poly.c
@@ -1,230 +1,131 @@
-
 #include <stdint.h>
-#include <string.h>
 #include <stdio.h>
 #include "params.h"
 
 #include "poly.h"
 
-//=============== Begin ===============
-typedef int64_t ll;
-#define Max 256
-const ll Mod = 689522689;//998244353;//689522689;//677795329;   // 模数
-const ll SpMul = 45903;//3;//34286;//45903;//34286;  // 原根
-ll twiddles[7] = { 689522688, 601244985, 421426171, 167518745, 136421068, 283562815, 497758789, 298752573};
-ll inv_twiddles[7] = { 689522688, 88277704, 38177824, 325982118, 112842325, 161291536, 457305796, 220289161};
+int32_t mu = (1UL << 16) / NTRUP_Q;
 
-static int64_t bred(int64_t a, int32_t m, uint64_t mu);
-
-uint64_t mu_big = (1UL << 32) / Mod;
-void swap(ll *a, ll *b)
-{
-	ll tmp = *a;
-	*a = *b;
-	*b = tmp;
-}
-
-ll qpow(ll a, ll k) {
-    ll res = 1;
-    while (k > 0) {
-        if (k & 1)res = res * a % Mod;
-        a = a * a % Mod;
-        k >>= 1;
-    }
-    return res;
-}
-
-void Change(ll y[], int len) {
-    for (int i = 1, j = len / 2; i < len - 1; i++) {
-        if (i < j)swap(&y[i], &y[j]);
-        int k = len / 2;
-        while (j >= k) {
-            j -= k;
-            k /= 2;
-        }
-        if (j < k)j += k;
-    }
-}
-
-
-void NTT(ll y[], int len, int on) {
-    Change(y, len);
-    /*
-	if(on == 1) {
-		printf("twiddles = { ");
-	}else{
-		printf("inv_twiddles = { ");
-	}
-	*/
-	for (int h = 2, tw_i = 0; h <= len; h <<= 1) {
-		ll wn;
-		if (on == 1) wn = twiddles[tw_i++];
-		else wn = inv_twiddles[tw_i++];
-		//if (on == -1)wn = qpow(wn, Mod - 2);
-        //printf("%ld, ", wn);
-		for (int j = 0; j < len; j += h) {
-            ll w = 1LL;
-            for (int k = j; k < j + h / 2; k++) {
-                ll u = y[k];
-                ll t = bred(w * y[k + h / 2], Mod, mu_big);
-                y[k] = bred((u + t), Mod, mu_big);
-                y[k + h / 2] = bred((u - t + Mod), Mod, mu_big);
-                w = bred(w * wn, Mod, mu_big);
-            }
-        }
-    }
-	//printf("}\n");
-    if (on == -1) {
-        ll t = qpow(len, Mod - 2);
-        for (int i = 0; i < len; i++)
-            y[i] = bred((y[i] * t), Mod, mu_big);
-    }
-}
-
-ll data1[6][Max], data2[6][Max];
-ll tmp[36][Max];
-//===============  End ===============
-
-
-
-static int64_t bred(int64_t a, int32_t m, uint64_t mu) {
-    int64_t q = ((uint64_t)a * mu) >> 32;
-    int64_t t = a - q * m;
-    if (t > (m >> 1)) {
-        t -= m;
-    }
-    if (t < -(m >> 1)) {
-        t += m;
-    }
-    return t;
-}
-
-static int16_t bred_smallm(int64_t a, int16_t m, uint32_t mu) {
-    int64_t q = ((uint64_t)a * mu) >> 16;
-    int64_t t = a - q * m;
-    if (t > (m >> 1)) {
-        t -= m;
-    }
-    if (t < -(m >> 1)) {
-        t += m;
-    }
-    return t;
-}
-
-static int16_t cmod(int32_t a, int16_t m){
+static int16_t cmod(int32_t a, int16_t mod){
     int16_t t;
-    t = a % m;
-    if(t > (m >> 1)){
-        t -= m;
+    t = a % mod;
+    if(t > (mod >> 1)){
+        t -= mod;
     }
-    if(t < -(m >> 1)){
-        t += m;
+    if(t < -(mod >> 1)){
+        t += mod;
     }
     return t;
+}
+int32_t m = (int32_t)NTRUP_Q;
+static int16_t bred(int32_t a) {
+    int32_t q = ((int32_t)a * mu) >> 16;
+    int32_t t = a - q * m;
+    if (t > (m >> 1)) {
+        t -= m;
+    }
+    if (t < -(m >> 1)) {
+        t += m;
+    }
+    return (int16_t)t;
 }
 
 
 void poly_Rq_mul_small(int16_t *h, const int16_t *f,const int8_t *g)
 {
-	
-	uint32_t mu = (1UL << 16) / NTRUP_Q;
-	//1. Initialize
-	int16_t len = 128;
-	
-	for(int16_t d = 0; d < 5; d++){
-		memset(data1[d], 0, sizeof(data1[0]));
-		for(int16_t i = 0; i < len; i++){
-			data1[d][i] = f[d*len + i];
-		}
-	}/*
-	for(int i = 0; i < 10; i++){
-		printf("data1[0][%d] = %ld\n", i, data1[0][i]);
-	}
-	*/
-	//printf("============\n");
-	memset(data1[5], 0, sizeof(data1[0]));
-	for(int16_t i = 0; i < 121; i++){
-		data1[5][i] = f[640+i];
-	}
-	
-	for(int16_t d = 0; d < 5; d++){
-		memset(data2[d], 0, sizeof(data2[0]));
-		for(int16_t i = 0; i < len; i++){
-			data2[d][i] = g[d*len + i];
-		}
-	}
-	memset(data2[5], 0, sizeof(data2[0]));
-	for(int16_t i = 0; i < 121; i++){
-		data2[5][i] = g[640+i];
-	}
-	int16_t len256 = 256;
-	
-	//2. NTT
-    for(int16_t i = 0; i < 6; i++){
-    	NTT(data1[i], len256, 1);
+    int16_t fg[NTRUP_P + NTRUP_P - 1];
+    int32_t result, r1, r2, r3, r4, r5, r6, r7, r8;
+	int i,j;
+	//h[0]
+	fg[0] = f[0]*g[0];
+	fg[1] = f[1]*(int32_t)g[0] + f[0]*(int32_t)g[1];
+	fg[2] = bred(f[2]*(int32_t)g[0] + f[1]*(int32_t)g[1] + f[0]*(int32_t)g[2]);
+	fg[3] = bred(f[3]*(int32_t)g[0] + f[2]*(int32_t)g[1] + f[1]*(int32_t)g[2] + f[0]*(int32_t)g[3]);
+	fg[4] = bred(f[4]*(int32_t)g[0] + f[3]*(int32_t)g[1] + f[2]*(int32_t)g[2] 
+				+ f[1]*(int32_t)g[3] + f[0]*(int32_t)g[4]);
+	fg[5] = bred(f[5] * (int32_t)g[0] + f[4] * (int32_t)g[1] + f[3] * (int32_t)g[2] + 
+				f[2] * (int32_t)g[3] + f[1] * (int32_t)g[4] + f[0] * (int32_t)g[5]);
+	fg[6] = bred(f[6] * (int32_t)g[0] + f[5] * (int32_t)g[1] + f[4] * (int32_t)g[2] + 
+				f[3] * (int32_t)g[3] + f[2] * (int32_t)g[4] + f[1] * (int32_t)g[5] + 
+				f[0] * (int32_t)g[6]);
+	int16_t rem = 0;
+	for (i = 7; i < NTRUP_P; i++) {
+      result = 0;
+      for (j = 0;j <= i;j+=7){
+	  	  rem = i - j;
+		  if(rem < 6){
+			while(rem >= 0){	
+				result = result + (int32_t)(f[i-rem]*(int32_t)g[rem]);
+				rem--;
+			}	
+			result = bred(result);
+			break;
+		  }
+	  	  r1 = (int32_t)f[j]*(int32_t)g[i-j];
+	  	  r2 = (int32_t)f[j+1]*(int32_t)g[i-j-1];
+	  	  r3 = (int32_t)f[j+2]*(int32_t)g[i-j-2];
+	  	  r4 = (int32_t)f[j+3]*(int32_t)g[i-j-3];
+
+	  	  r5 = (int32_t)f[j+4]*(int32_t)g[i-j-4];
+	  	  r6 = (int32_t)f[j+5]*(int32_t)g[i-j-5];
+	  	  r7 = (int32_t)f[j+6]*(int32_t)g[i-j-6];
+	  	  //r8 = (int32_t)(f[j+7]*(int32_t)g[i-j-7]);
+		  		   
+          result = bred(result+r1+r2+r3+r4+r5+r6+r7);
+	  }
+      fg[i] = result;
+
     }
-	for(int16_t i = 0; i < 6; i++){
-    	NTT(data2[i], len256, 1);
-    }
-    /*
-    for(int i = 0; i < 10; i++){
-		printf("data1[0][%d] = %ld\n", i, data1[0][i]);
-	}
-	*/
-	//3. element-wise product
-	for(int16_t i = 0; i < 6; i++){
-		for(int16_t j = 0; j < 6; j++){
-			for(int16_t e = 0; e < len256; e++){
-				tmp[j + 6*i][e] = bred(data1[i][e] * data2[j][e] , Mod, mu_big);//(data1[i][e] * data2[j][e]) % Mod;//bred(data1[i][e] * data2[j][e] , Mod, mu_big);
-			}
-		}
-	}
 	
-	//4. INTT
-	for(int16_t i = 0; i < 36; i++){
-		NTT(tmp[i], len256, -1);
-	}
-	int32_t tmp_h[(len<<3) + (len<<2)];
-	//5. Construct h from tmp
-	for(int16_t i = 0; i < 6; i++){
-		for(int16_t j = 0; j < 6; j++){
-			for(int16_t e = 0; e < len256; e++){
-				tmp_h[((i + j)<<7) + e] = bred_smallm(tmp[(i<<2) + (i<<1) + j][e], NTRUP_Q, mu); //tmp[(i<<2) + (i<<1) + j][e] % NTRUP_Q;//bred(tmp[(i<<2) + (i<<1) + j][e], NTRUP_Q, mu); 
-			}
-		}	
-	}
 	/*
-	for(int i = 0; i < 10; i++){
-		printf("tmp_h[%d]= %ld\n", i, tmp_h[i]);
-	}
-	*/
-	for (int i = 2 * NTRUP_P - 2; i >= NTRUP_P; i--) {
-      h[i - NTRUP_P] = bred_smallm(tmp_h[i - NTRUP_P] + tmp_h[i], NTRUP_Q, mu);//(tmp_h[i - NTRUP_P] + tmp_h[i]) % NTRUP_Q;//bred_smallm(tmp_h[i - NTRUP_P] + tmp_h[i], NTRUP_Q, mu);
-      h[i - NTRUP_P + 1] = (tmp_h[i - NTRUP_P + 1] + tmp_h[i]) % NTRUP_Q;//bred_smallm(tmp_h[i - NTRUP_P + 1] + tmp_h[i], NTRUP_Q, mu);
+	for (i = 0; i < NTRUP_P; i++) {
+      result = 0;
+      for (j = 0;j <= i;++j) 
+          result = bred(result+f[j]*(int32_t)g[i-j]);
+      fg[i] = result;
     }
-	
+	*/
+	/*
+	for (i = NTRUP_P;i < 2 * NTRUP_P - 1;++i) {
+      result = 0;
+      for (j = i - NTRUP_P + 1; j < NTRUP_P; ++j)
+          result = bred(result+f[j]*(int32_t)g[i-j]);
+      fg[i] = result;
+    }
+    */
+	for (i = NTRUP_P;i < 2 * NTRUP_P - 1;++i) {
+      result = 0;
+      for (j = i - NTRUP_P + 1; j < NTRUP_P; j+=7) { 
+		  if(j > NTRUP_P-7){
+			while(j < NTRUP_P){	
+				result = result + (int32_t)(f[j]*(int32_t)g[i - j]);
+				j++;
+			}	
+			result = bred(result);
+			break;
+		  }
+	  	  r1 = (int32_t)f[j]*(int32_t)g[i-j];
+	  	  r2 = (int32_t)f[j+1]*(int32_t)g[i-j-1];
+	  	  r3 = (int32_t)f[j+2]*(int32_t)g[i-j-2];
+	  	  r4 = (int32_t)f[j+3]*(int32_t)g[i-j-3];
+
+	  	  r5 = (int32_t)f[j+4]*(int32_t)g[i-j-4];
+	  	  r6 = (int32_t)f[j+5]*(int32_t)g[i-j-5];
+	  	  r7 = (int32_t)f[j+6]*(int32_t)g[i-j-6];
+	  	  //r8 = (int32_t)(f[j+7]*(int32_t)g[i-j-7]);
+          result = bred(result+r1+r2+r3+r4+r5+r6+r7);
+
+          //result = bred(result+f[j]*(int32_t)g[i-j]);
+      }
+      fg[i] = result;
+    }
+
+
+    for (i = 2 * NTRUP_P - 2; i >= NTRUP_P; i--) {
+      fg[i - NTRUP_P] = (int16_t)bred(fg[i - NTRUP_P] + fg[i]);
+	  fg[i - NTRUP_P + 1] = (int16_t)bred(fg[i - NTRUP_P + 1] + fg[i]);
+    }
+
+    for (i = 0; i < NTRUP_P; ++i) 
+        h[i] = fg[i];
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
